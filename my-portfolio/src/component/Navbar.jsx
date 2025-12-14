110f3e7cd5eb7c1fc29c053ffda4e5b0d7685e63
sourav030 <128700861+sourav030@users.noreply.github.com>
2025-12-15 00:27:59 +0530

    debug it

--- my-portfolio/src/component/Navbar.jsx
+++ my-portfolio/src/component/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaSun, FaMoon } from 'react-icons/fa';
-import useTheme from '../hooks/useTheme';
+
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
